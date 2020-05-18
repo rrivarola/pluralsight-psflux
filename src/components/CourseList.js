03dda9c67273547c7df4b1b123b5bdbc7b3ab4ec
Raúl Rivarola <raul.rivarola@netviax.com>
2020-05-18 17:32:46 -0300

    PropTypes

---
+++ src/components/CourseList.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { prototype } from "flux/lib/Dispatcher";
+
+function CourseList(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Tilte</th>
+          <th>Author</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.courses.map((course) => {
+          return (
+            <tr key={course.id}>
+              <td>{course.title}</td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+CourseList.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).array.isRequired,
+};
+
+export default CourseList;
