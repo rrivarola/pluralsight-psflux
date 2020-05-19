a06a4061ec813938fe15a28b1e2bfda9eb51062e
Raúl Rivarola <raul.rivarola@netviax.com>
2020-05-18 22:34:07 -0300

    router

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,22 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/courses") return <CoursesPage />;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
-
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/courses" component={CoursesPage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+        <Route path="/course/:slug" component={ManageCoursePage}></Route>
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage}></Route>
+      </Switch>
     </div>
   );
 }
